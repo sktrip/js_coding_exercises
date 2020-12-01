9b9c7b46d832e56f0ca1386520d064d43dd3d42c
sktrip <sktripurari@gmail.com>
2020-12-01 19:05:14 +0000

    exercise002 Completed
    Removed comments and formatted the file.

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,29 +1,25 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
   return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
   return person.city == 'Manchester';
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
   const busCapacity = 40;
-  var numberOfBuses = Math.ceil(people/busCapacity);
-  return numberOfBuses;  
+  let numberOfBuses = Math.ceil(people / busCapacity);
+  return numberOfBuses;
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
-  var sheepCount = 0;
-  for(var index=0; index < arr.length; index++) {
-    if(arr[index]=='sheep') {
+  let sheepCount = 0;
+  for (let index = 0; index < arr.length; index++) {
+    if (arr[index] == 'sheep') {
       sheepCount += 1;
     }
   } return sheepCount;
@@ -31,8 +27,7 @@ function countSheep(arr) {
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
-  return ((person.address.postCode.startsWith('M')) && (person.address.city=='Manchester'));
+  return ((person.address.postCode.startsWith('M')) && (person.address.city == 'Manchester'));
 }
 
 module.exports = {
