f41ecbfbaafe7752a4ca1f8f974d84cefa57dfa6
sktrip <sktripurari@gmail.com>
2020-12-14 22:27:00 +0000

    exercise 006- Completed

--- challenges/exercise006.js
+++ challenges/exercise006.js
@@ -43,8 +43,6 @@ const getComplementaryDNA = str => {
 const isItPrime = n => {
   if (n === undefined) throw new Error("n is required");
   if (n < 2) return false;
-  //   if(n==2) return true;
-  //   if (n % 2 == 0) return false;
   for (var i = 2; i <= Math.sqrt(n); ++i) 
     if (n % i == 0) return false;
   return true;
